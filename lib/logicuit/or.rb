a3a374d74f901250517af0e3aa18e679b6ac57c0
Koji NAKAMURA <kozy4324@gmail.com>
2025-02-07 13:20:14 +0900

    Make signal method lazy-evaluated

--- lib/logicuit/or.rb
+++ lib/logicuit/or.rb
@@ -4,12 +4,14 @@ module Logicuit
   # OR circuit
   class Or
     def initialize(a, b) # rubocop:disable Naming/MethodParameterName
-      @a = a
-      @b = b
+      @a = a.respond_to?(:call) ? a : -> { a }
+      @b = b.respond_to?(:call) ? b : -> { b }
     end
 
     def signal
-      [@a == 1 || @b == 1 ? 1 : 0]
+      a = @a.call
+      b = @b.call
+      [a == 1 || b == 1 ? 1 : 0]
     end
   end
 end
--- test/test_and.rb
+++ test/test_and.rb
@@ -3,10 +3,15 @@
 require "test_helper"
 
 class TestAnd < Minitest::Test
-  def test_signal
+  def test_signal # rubocop:disable Metrics/AbcSize
     assert_equal [1], Logicuit::And.new(1, 1).signal
     assert_equal [0], Logicuit::And.new(1, 0).signal
     assert_equal [0], Logicuit::And.new(0, 1).signal
     assert_equal [0], Logicuit::And.new(0, 0).signal
+
+    assert_equal [1], Logicuit::And.new(-> { 1 }, -> { 1 }).signal
+    assert_equal [0], Logicuit::And.new(-> { 1 }, -> { 0 }).signal
+    assert_equal [0], Logicuit::And.new(-> { 0 }, -> { 1 }).signal
+    assert_equal [0], Logicuit::And.new(-> { 0 }, -> { 0 }).signal
   end
 end
