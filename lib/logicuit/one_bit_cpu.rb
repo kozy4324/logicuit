cb96a4c5c0ad89f73d4f3b806fa718e89d6e0bc4
Koji NAKAMURA <kozy4324@gmail.com>
2025-02-26 10:36:41 +0900

    Implement OneBitCpu

---
+++ lib/logicuit/one_bit_cpu.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Logicuit
+  # 1 bit CPU
+  #
+  #  +-(Y)-|NOT|-(A)-+
+  #  |               |
+  #  +-(D)-|   |-(Q)-+
+  #        |DFF|
+  #   (CK)-|>  |
+  #
+  class OneBitCpu
+    def initialize(ck = nil)
+      @dff = Logicuit::DFlipFlop.new(0, ck)
+      @not = Logicuit::Not.new
+      @dff.q >> @not.a
+      @not.y >> @dff.d
+    end
+
+    def to_s
+      <<~CIRCUIT
+        +-(#{@not.y})-|NOT|-(#{@not.a})-+
+        |               |
+        +-(#{@dff.d})-|   |-(#{@dff.q})-+
+              |DFF|
+         (CK)-|>  |
+      CIRCUIT
+    end
+  end
+end
