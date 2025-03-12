67babf10e6317f2378eda85f4318168a88a17dc7
Koji NAKAMURA <kozy4324@gmail.com>
2025-03-12 12:57:40 +0900

    v0.1.3

--- test/test_logicuit.rb
+++ test/test_logicuit.rb
@@ -4,6 +4,6 @@ require "test_helper"
 
 class LogicuitTest < Minitest::Test
   def test_that_it_has_a_version_number
-    assert_equal "0.1.2", ::Logicuit::VERSION
+    assert_equal "0.1.3", ::Logicuit::VERSION
   end
 end
