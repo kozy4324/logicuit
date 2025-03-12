2d39b231a681fec97dfcef8d5d6395d3925ce44f
Koji NAKAMURA <kozy4324@gmail.com>
2025-03-12 12:51:18 +0900

    Implement Multiplexer4To1

---
+++ lib/logicuit/circuits/combinational/multiplexer4to1.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+module Logicuit
+  module Circuits
+    module Combinational
+      # A Multiplexer with 4 inputs and 1 output
+      class Multiplexer4To1 < Base
+        define_inputs :c0, :c1, :c2, :c3, :b, :a
+
+        define_outputs y: lambda { |c0, c1, c2, c3, b, a|
+          (c0 && !b && !a) || (c1 && !b && a) || (c2 && b && !a) || (c3 && b && a)
+        }
+
+        diagram <<~DIAGRAM
+          (C0)---------------|
+                         +---|AND|-----|
+                         | +-|         |
+                         | |           |
+          (C1)---------------|         |
+                         +---|AND|-----|
+                 +-----------|         |
+                 |       | |           |OR|--(Y)
+          (C2)---------------|         |
+               +-------------|AND|-----|
+               | |       | +-|         |
+               | |       | |           |
+          (C3)---------------|         |
+               +-------------|AND|-----|
+               | +-----------|
+               | |       | |
+          (B)--+---|NOT|-+ |
+                 |         |
+          (A)----+-|NOT|---+
+        DIAGRAM
+
+        truth_table <<~TRUTH_TABLE
+          | B | A | C0 | C1 | C2 | C3 | Y |
+          | - | - | -- | -- | -- | -- | - |
+          | 0 | 0 |  0 |  x |  x |  x | 0 |
+          | 0 | 0 |  1 |  x |  x |  x | 1 |
+          | 0 | 1 |  x |  0 |  x |  x | 0 |
+          | 0 | 1 |  x |  1 |  x |  x | 1 |
+          | 1 | 0 |  x |  x |  0 |  x | 0 |
+          | 1 | 0 |  x |  x |  1 |  x | 1 |
+          | 1 | 1 |  x |  x |  x |  0 | 0 |
+          | 1 | 1 |  x |  x |  x |  1 | 1 |
+        TRUTH_TABLE
+      end
+    end
+  end
+end
