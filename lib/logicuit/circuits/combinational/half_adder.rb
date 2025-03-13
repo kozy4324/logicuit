2ad2e59ec585dfb28da68c164a43e295e35328cf
Koji NAKAMURA <kozy4324@gmail.com>
2025-03-13 12:56:56 +0900

    Temporarily implemented the HalfAddr class

--- lib/logicuit/circuits/combinational/half_adder.rb
+++ lib/logicuit/circuits/combinational/half_adder.rb
@@ -1,3 +1,31 @@
 # frozen_string_literal: true
 
-# TODO: implement the HalfAdder class
+module Logicuit
+  module Circuits
+    module Combinational
+      # HalfAdder class
+      class HalfAdder < Base
+        define_inputs :a, :b
+
+        define_outputs c: lambda { |a, b|
+          true # TODO: Implement the logic
+        }, s: lambda { |a, b|
+          true # TODO: Implement the logic
+        }
+
+        diagram <<~DIAGRAM
+          TODO: Implement the diagram
+        DIAGRAM
+
+        truth_table <<~TRUTH_TABLE
+          | A | B | C | S |
+          | - | - | - | - |
+          | 0 | 0 | 0 | 0 |
+          | 0 | 1 | 0 | 1 |
+          | 1 | 0 | 0 | 1 |
+          | 1 | 1 | 1 | 0 |
+        TRUTH_TABLE
+      end
+    end
+  end
+end
