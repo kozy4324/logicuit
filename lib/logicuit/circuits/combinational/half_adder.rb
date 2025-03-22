c5397dd8d6030773049aa74cd6c0ed1e77bacbf9
Koji NAKAMURA <kozy4324@gmail.com>
2025-03-22 10:46:41 +0900

    Fix diagram of HalfAdder

--- lib/logicuit/circuits/combinational/half_adder.rb
+++ lib/logicuit/circuits/combinational/half_adder.rb
@@ -10,7 +10,7 @@ module Logicuit
         diagram <<~DIAGRAM
           (A)---+-|
                 | |XOR|-(S)
-          (B)-+---+
+          (B)-+---|
               | |
               | +-|
               |   |AND|-(C)
