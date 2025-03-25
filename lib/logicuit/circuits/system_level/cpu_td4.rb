c9578adff5937c7e3c197ba2e8ef100a2a352911
Koji NAKAMURA <kozy4324@gmail.com>
2025-03-26 06:42:36 +0900

    First commit of TD4

--- lib/logicuit/circuits/system_level/cpu_td4.rb
+++ lib/logicuit/circuits/system_level/cpu_td4.rb
@@ -7,9 +7,9 @@ module Logicuit
       class CpuTd4 < Base
         tag :TD4
 
-        define_inputs ld0, ld1, ld2, ld3, sel_a, sel_b, im_d0, im_d1, im_d2, im_d3, clock: :ck
+        define_inputs :ld0, :ld1, :ld2, :ld3, :sel_a, :sel_b, :im_d0, :im_d1, :im_d2, :im_d3, clock: :ck
 
-        assembling do |ld0, ld1, ld2, ld3, sel_a, sel_b, im_d0, im_d1, im_d2, im_d3|
+        assembling do |ld0, ld1, ld2, ld3, sel_a, sel_b, im_d0, im_d1, im_d2, im_d3| # rubocop:disable Metrics/ParameterLists
           # TODO: Implement assembling
         end
       end
