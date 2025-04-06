5c0afd408ac5868bae3c8c7039b7116009c13c44
Koji NAKAMURA <kozy4324@gmail.com>
2025-04-06 12:43:54 +0900

    Initial implementation of Decoder

---
+++ lib/logicuit/circuits/combinational/decoder.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Logicuit
+  module Circuits
+    module Combinational
+      # HalfAdder class
+      class Decoder < Base
+        tag :DEC
+
+        diagram <<~DIAGRAM
+          TODO: implement decoder diagram
+        DIAGRAM
+
+        define_inputs :op3, :op2, :op1, :op0, :c_flag
+
+        define_outputs :sel_b, :sel_a, :ld0, :ld1, :ld2, :ld3
+
+        truth_table <<~TRUTH_TABLE
+          | OP3 | OP2 | OP1 | OP0 | C_FLAG | SEL_B | SEL_A | LD0 | LD1 | LD2 | LD3 |
+          | --- | --- | --- | --- | ------ | ----- | ----- | --- | --- | --- | --- |
+          |   0 |   0 |   0 |   0 |      x |     0 |     0 |   0 |   1 |   1 |   1 |
+        TRUTH_TABLE
+      end
+    end
+  end
+end
