f14299f4c61de3acc8ed0bf7f33ff4635ddd7a5a
Koji NAKAMURA <kozy4324@gmail.com>
2025-04-07 09:18:56 +0900

    Implement outputs of Decoder

--- lib/logicuit/circuits/combinational/decoder.rb
+++ lib/logicuit/circuits/combinational/decoder.rb
@@ -13,7 +13,12 @@ module Logicuit
 
         define_inputs :op3, :op2, :op1, :op0, :c_flag
 
-        define_outputs :sel_b, :sel_a, :ld0, :ld1, :ld2, :ld3
+        define_outputs sel_b: ->(op3, op2, op1, c_flag) { op1 }, # rubocop:disable Lint/UnusedBlockArgument
+                       sel_a: ->(op3, op2, op1, c_flag) { op3 || op0 }, # rubocop:disable Lint/UnusedBlockArgument
+                       ld0: ->(op3, op2, op1, c_flag) { op3 || op2 }, # rubocop:disable Lint/UnusedBlockArgument
+                       ld1: ->(op3, op2, op1, c_flag) { op3 || !op2 }, # rubocop:disable Lint/UnusedBlockArgument
+                       ld2: ->(op3, op2, op1, c_flag) { !op3 || op2 }, # rubocop:disable Lint/UnusedBlockArgument
+                       ld3: ->(op3, op2, op1, c_flag) { !op3 || !op2 || (!op0 && c_flag) } # rubocop:disable Lint/UnusedBlockArgument
 
         truth_table <<~TRUTH_TABLE
           | OP3 | OP2 | OP1 | OP0 | C_FLAG | SEL_B | SEL_A | LD0 | LD1 | LD2 | LD3 |
