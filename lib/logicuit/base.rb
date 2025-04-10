c266a08102c08620beaa2960aa8eba7185cbb3cf
Koji NAKAMURA <kozy4324@gmail.com>
2025-04-10 22:53:10 +0900

    Refactor: add comment

--- lib/logicuit/base.rb
+++ lib/logicuit/base.rb
@@ -33,6 +33,7 @@ module Logicuit
     attr_reader :input_targets, :output_targets, :clock, :components, :initialized
 
     def self.define_inputs(*args, **kwargs)
+      # define getter methods for inputs
       attr_reader(*args)
 
       # define initializer for inputs
