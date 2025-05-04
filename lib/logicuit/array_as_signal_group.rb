200ecafbdef824571aac614315f50777701ff5e4
Koji NAKAMURA <kozy4324@gmail.com>
2025-05-05 06:48:46 +0900

    Convert RBS description of array_as_signal_group.rb to RBS-inline

--- lib/logicuit/array_as_signal_group.rb
+++ lib/logicuit/array_as_signal_group.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 
+# rbs_inline: enabled
+
 # Logicuit module
 module Logicuit
   # Treats Array#>> as SignalGroup#>> for the purpose of connecting signals
   module ArrayAsSignalGroup
     refine Array do
+      #: def >>: (untyped other) -> void
       def >>(other)
         Signals::SignalGroup.new(*self).connects_to(other) # steep:ignore
       end
     end
   end
 end
+
+# @rbs!
+#   class Array[unchecked out T]
+#     def >>: (untyped other) -> void
+#   end
