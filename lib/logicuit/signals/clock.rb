d3322f23c66bf430f699efe192dc464ef76310ef
Koji NAKAMURA <kozy4324@gmail.com>
2025-05-06 10:43:06 +0900

    Convert RBS description of clock.rb to RBS-inline

--- lib/logicuit/signals/clock.rb
+++ lib/logicuit/signals/clock.rb
@@ -1,16 +1,25 @@
 # frozen_string_literal: true
 
+# rbs_inline: enabled
+
 module Logicuit
   module Signals
     # Clock
     class Clock
+      #: def initialize: () -> void
       def initialize
+        #: @downstreams: Array[DSL]
         @downstreams = []
+        #: @tick_count: Integer
         @tick_count = 0
       end
 
+      # @rbs!
+      #   attr_reader downstreams: Array[DSL]
+      #   attr_reader tick_count: Integer
       attr_reader :downstreams, :tick_count
 
+      #: def tick: () -> void
       def tick
         @tick_count += 1
         # Call the `evaluate` method for all components.
@@ -22,19 +31,24 @@ module Logicuit
       end
 
       class << self
+        #: def self.instance: () -> Clock
         def instance
+          #: self.@instance: Clock
           @instance ||= new
         end
 
+        #: def self.connects_to: (DSL component) -> void
         def connects_to(component)
           instance.downstreams << component
         end
         alias >> connects_to
 
+        #: def self.tick: () -> void
         def tick
           instance.tick
         end
 
+        #: def self.tick_count: () -> Integer
         def tick_count
           instance.tick_count
         end
