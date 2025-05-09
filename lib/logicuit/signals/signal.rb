b9b9cad4f472c009850bcd1fb5e33e13b7327361
Koji NAKAMURA <kozy4324@gmail.com>
2025-05-09 11:56:29 +0900

    Convert RBS description of signal.rb to RBS-inline

--- lib/logicuit/signals/signal.rb
+++ lib/logicuit/signals/signal.rb
@@ -1,16 +1,22 @@
 # frozen_string_literal: true
 
+# rbs_inline: enabled
+
 module Logicuit
   module Signals
     # Signal
     class Signal
+      # @rbs @downstreams: Array[Signal | DSL]
+
+      #: (?bool current) -> void
       def initialize(current = false)
         @current = current
         @downstreams = []
       end
 
-      attr_reader :current
+      attr_reader :current #: bool
 
+      #: () -> void
       def on
         return if @current
 
@@ -18,6 +24,7 @@ module Logicuit
         propagate_current
       end
 
+      #: () -> void
       def off
         return unless @current
 
@@ -25,6 +32,7 @@ module Logicuit
         propagate_current
       end
 
+      #: (Signal | SignalGroup | Array[Signal] | DSL other) -> void
       def connects_to(other)
         if other.is_a? Array
           @downstreams.concat(other)
@@ -37,24 +45,32 @@ module Logicuit
       end
       alias >> connects_to
 
+      #: () -> ("1" | "0")
       def to_s
         current ? "1" : "0"
       end
 
+      # logical AND operation
+      #: (Signal other) -> Signal
       def &(other)
         Signal.new(current && other.current)
       end
 
+      # logical OR operation
+      #: (Signal other) -> Signal
       def |(other)
         Signal.new(current || other.current)
       end
 
+      # logical NOT operation
+      #: () -> Signal
       def !
         Signal.new(!current)
       end
 
       private
 
+      #: () -> void
       def propagate_current
         @downstreams.each do |downstream|
           if downstream.is_a?(Signal)
